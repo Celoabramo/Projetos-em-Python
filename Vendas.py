9fffe5a24b9558f65d03ad9328ec1055910c79b8
Marcelo Najjar Abramo Filho <marcelo.abramo08@gmail.com>
2026-05-14 18:56:56 -0300

    Update Vendas.py

--- Vendas.py
+++ Vendas.py
@@ -1,81 +1,81 @@
 Vendas = [
-    ["Vendedor1",[]],
-    ["Vendedor2",[]],
-    ["Vendedor3",[]]
+    ["Vendedor1", []],
+    ["Vendedor2", []],
+    ["Vendedor3", []]
 ]
 
-
-    
 while True:
     while True:
-        print ("DIA 1"
-            "\nQuem Vendeu?"
-        "\n1-Vendedor 1"
-        "\n2-Vendedor 2"
-        "\n3-Vendedor 3"
-        "\n4-Fechar o dia")
+        print("DIA 1"
+              "\nQuem Vendeu?"
+              "\n1-Vendedor 1"
+              "\n2-Vendedor 2"
+              "\n3-Vendedor 3"
+              "\n4-Fechar o dia")
 
         opcao = int(input("Opcao:"))
-        
-        if opcao ==1:
+
+        if opcao == 1:
             Vendas[0][1].append(["Dia1", float(input("VALOR VENDA:"))])
-        elif opcao ==2:
+        elif opcao == 2:
             Vendas[1][1].append(["Dia1", float(input("VALOR VENDA:"))])
-        elif opcao ==3:
+        elif opcao == 3:
             Vendas[2][1].append(["Dia1", float(input("VALOR VENDA:"))])
         else:
             break
 
     while True:
-        print ("DIA 2"
-            "\nQuem Vendeu?"
-        "\n1-Vendedor 1"
-        "\n2-Vendedor 2"
-        "\n3-Vendedor 3"
-        "\n4-Fechar o dia")
+        print("======================================="
+            "\nDIA 2"
+              "\nQuem Vendeu?"
+              "\n1-Vendedor 1"
+              "\n2-Vendedor 2"
+              "\n3-Vendedor 3"
+              "\n4-Fechar o dia")
 
         opcao = int(input("Opcao:"))
-        
-        if opcao ==1:
+
+        if opcao == 1:
             Vendas[0][1].append(["Dia2", float(input("VALOR VENDA:"))])
-        elif opcao ==2:
+        elif opcao == 2:
             Vendas[1][1].append(["Dia2", float(input("VALOR VENDA:"))])
-        elif opcao ==3:
+        elif opcao == 3:
             Vendas[2][1].append(["Dia2", float(input("VALOR VENDA:"))])
         else:
             break
 
     while True:
-        print ("DIA 3"
-            "\nQuem Vendeu?"
-        "\n1-Vendedor 1"
-            "\n2-Vendedor 2"
-            "\n3-Vendedor 3"
-            "\n4-Fechar o dia")
+        print("========================================"
+            "\nDIA 3"
+              "\nQuem Vendeu?"
+              "\n1-Vendedor 1"
+              "\n2-Vendedor 2"
+              "\n3-Vendedor 3"
+              "\n4-Fechar o dia")
 
         opcao = int(input("Opcao:"))
-        
-        if opcao ==1:
+
+        if opcao == 1:
             Vendas[0][1].append(["Dia3", float(input("VALOR VENDA:"))])
-        elif opcao ==2:
+        elif opcao == 2:
             Vendas[1][1].append(["Dia3", float(input("VALOR VENDA:"))])
-        elif opcao ==3:
+        elif opcao == 3:
             Vendas[2][1].append(["Dia3", float(input("VALOR VENDA:"))])
         else:
             break
-        
-    terminar = input("Reiniciar?(s/n):")
+
+    terminar = input("==========================================="
+        "\nReiniciar?(s/n):")
 
     if terminar == "n":
-        for j in range(len(Vendas)):
-            if len(Vendas[j][1]) <= 0:
-                print("SEM VENDAS!!")
-                break
-            for i in range(len(Vendas[j])):
-                for r in range(len(Vendas[j][1])):
-                    print(f"{Vendas[j][0]} - {Vendas[j][1][r]}")
+        if len(Vendas[0][1])+len(Vendas[1][1])+len(Vendas[2][1]) <= 0:
+            print("SEM VENDAS!!")
+            break
+        else:
+            for j in range(len(Vendas)):
+                print(f"{Vendas[j][0]} - {Vendas[j][1]}")
 
-        break
+            break
     else:
         Vendas[0].pop()
         Vendas[1].pop()
